b58cba7c89c28dde706e645e02276187c8103edf
VrajeshPatel02 <kevalatel2990@gmail.com>
2026-03-13 00:49:54 +0530

    updated app.py

--- app.py
+++ app.py
@@ -48,4 +48,6 @@ def predict():
     return render_template('index.html', prediction_text=prediction_text, output_array=output_array)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    # Render (and many other platforms) set the port via an environment variable
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
